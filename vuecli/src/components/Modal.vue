456bc888b5309cac305983ffc92d66aebe48b7b3
arkarhein <arkerhein44@gmail.com>
2025-08-29 23:16:28 +0630

    vue file structure

---
+++ vuecli/src/components/Modal.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1 ref="headingtext">I'm Modal.</h1>
+        <button @click="changeHeading">Change Heading</button>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    methods:{
+        changeHeading(){
+            this.$refs.headingtext.textContent = "Hello, Everyone";
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: aqua;
+        font-style: normal;
+    }
+</style>
