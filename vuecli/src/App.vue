0487cb994d695181e96600af97819e5db504796a
arkarhein <arkerhein44@gmail.com>
2025-08-31 00:03:25 +0630

    modal

--- vuecli/src/App.vue
+++ vuecli/src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    <h1>Hello</h1>
-    <Modal />
+  <div class="container">
+    
+    <Modal ref="info" header="" content="" theme=""/>
+    <Modal header="" content="" theme=""/>
+    <Modal header="" content="" theme=""/>
+
+    <div>
+      <button class="btn btn-info" @click="infoModal">Info Modal</button>
+      <button class="btn btn-success">Success Modal</button>
+      <button class="btn btn-danger">Danger Modal</button>
+    </div>
+    
   </div>
   
 </template>
@@ -11,10 +20,45 @@ import Modal from "./components/Modal.vue";
 export default {
   components:{
     Modal
+  },
+  methods:{
+    infoModal(){
+      this.$refs.info.style.display = "none";
+    }
   }
 }
 </script>
 
 <style>
-  
+  .container{
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .btn{
+    padding: 10px;
+    margin: 10px;
+
+    color: white;
+
+    outline: none;
+    border-radius: 5px;
+    border: none;
+
+    cursor: pointer;
+  }
+
+  .btn-info{
+    background-color: rgb(52, 99, 185);
+  }
+
+  .btn-success{
+    background-color: rgb(52, 185, 81);
+  }
+
+  .btn-danger{
+    background-color: rgb(185, 52, 52);
+  }
 </style>
--- vuecli/src/components/Modal.vue
+++ vuecli/src/components/Modal.vue
@@ -1,24 +1,137 @@
 <template>
-    <div>
-        <h1 ref="headingtext">I'm Modal.</h1>
-        <button @click="changeHeading">Change Heading</button>
+    <div class="backdrop active">
+        <div class="modal info">
+            <div class="modal-header">
+                <h4>Alert</h4>
+            </div>
+            
+            <div class="modal-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, molestiae necessitatibus. Pariatur explicabo eum assumenda aperiam perspiciatis natus facilis, quibusdam maxime ratione nemo eaque ut quia id! Veniam, quos doloremque!</p>
+            </div>
+            
+            <div class="modal-footer">
+                <button>Ok</button>
+                <button>Cancel</button>
+            </div>
+        </div>
     </div>
-  
 </template>
 
 <script>
 export default {
-    methods:{
-        changeHeading(){
-            this.$refs.headingtext.textContent = "Hello, Everyone";
-        }
-    }
+   
 }
 </script>
 
 <style scoped>
-    h1{
-        color: aqua;
-        font-style: normal;
+    .backdrop{
+        margin: 0;
+        padding-top: 30px;
+
+        background-color: hsla(0, 0%, 0%, 0.5);
+        
+        width: 100vw;
+        height: 100vh;
+
+        position: fixed;
+
+        display: none;
     }
+    
+    .modal{
+        
+        margin: auto;
+
+        width: 400px;
+        border: 2px solid rgb(117, 117, 119);
+        border-radius: 5px;
+    }
+
+    .modal-header{  
+        padding: 10px 0;      
+        text-align: center;
+        border-bottom: 1px solid rgb(153, 146, 146);
+    }
+
+    h4{
+        margin: 0;
+        padding: 0;
+
+        font-size: 2rem;
+    }
+
+    .modal-body{
+        margin: 0;
+        padding:5px 30px;
+        text-align: justify;
+    }
+
+    p{
+        font-size: 1rem;
+    }
+
+    .modal-footer{
+        padding: 10px 0;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        text-align: center;
+
+        border-top: 1px solid rgb(153, 146, 146);
+    }
+
+    button{
+        padding: 10px;
+        margin: 5px;
+
+        width: 100px;
+        
+        color: white;
+
+        outline: none;
+        border-radius: 5px;
+        border: none;
+
+        cursor: pointer;
+    }
+
+    .info{
+        background-color: rgb(168, 215, 247);
+    }
+
+    .info h4, .info p{
+        color: rgb(27, 47, 90);
+    }
+
+    .info button{
+        background-color: rgb(52, 99, 185);
+    }
+
+
+    .success{
+        background-color: rgb(137, 253, 152);
+    }
+
+    .success h4, .success p{
+        color: rgb(32, 90, 27);
+    }
+
+    .success button{
+        background-color: rgb(52, 185, 81);
+    }
+
+    .danger{
+        background-color: rgb(243, 168, 168);
+    }
+
+    .danger h4, .danger p{
+        color: rgb(90, 27, 27);
+    }
+
+    .danger button{
+        background-color: rgb(185, 52, 52);
+    }
+
 </style>
